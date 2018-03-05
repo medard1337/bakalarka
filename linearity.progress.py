906097a5bd235f77938e2cf404526b6c888a4bed
medard1337 <miso.florek@gmail.com>
2018-03-05 10:10:01 +0100

    modified:   linearity.progress.py

--- linearity.progress.py
+++ linearity.progress.py
@@ -9,14 +9,13 @@ import decimal
 
 #otvori subor
 filename=input('Enter filename ')
-pocdlzka=float(input("Zadaj povodnu dlzku meranej vzorky: "))
 rawfile = open(filename, 'r')
 #deklaruje prazdny list s nazvom points
 points = []
 vydelene = []
 sigma = []
 delta = []
-gama = []
+
 
 #tato funkcia premiena string na float
 def tofloat(string):
@@ -50,70 +49,22 @@ for vydelene, i in enumerate(points):
 	if i==0:
 		print("0")
 	else:
-		if vydelene>=len(points)-4:
+		if vydelene>=len(points)-1:
 			print("end")
 		else:
-			if(abs(i-points[vydelene+4]))/i <= 0.03:
-				print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+4]))/i,"\t",'\t\tsedi\n')
+			if(abs(i-points[vydelene+1]))/i <= 0.03:
+				print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+1]))/i,"\t",'\t\tsedi\n')
 			else:
-				print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+4]))/i,'\t','\t\tnesedi\n')
+				print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+1]))/i,'\t','\t\tnesedi\n')
 
 #najde medzu pevnosti a maximalne predlzenie	
-print('\n------------------------------\n Medza pevnosti =', max(sigma,key=float),'MPa \n------------------------------\n')
-print('*Taznost je iba informativna*\n------------------------------\n Taznost je :', ceil(((((pocdlzka + abs(max(delta, key=float))) - pocdlzka) * 100) / pocdlzka) * 100) / 100.0, '%')
 
-#pocdlzka=float(input("Zadaj povodnu dlzku meranej vzorky: "))
-#taznost= ((('max_in_list(deformacia)') - pocdlzka) * 100)/pocdlzka
-#print('Taznost je :', taznost)
-	
+print('*Taznost je iba informativna*\n---------------------------------\n Taznost je :',(round(max(delta,key=float),5))*100,'%\n---------------------------------\n')
 
-#taznost=(((max_in_list(deformacia)-pocdlzka)/pocdlzka)*100)
-#print('Taznost =', taznost, '%')
-#def max_in_napatie(napatie):
-#	maxVal=napatie[0]
-##		if i>maxVal:
-#			maxVal = i
-##	return maxVal
-#print(max(napatie, key=float))
-#print(max_in_napatie(napatie))
-				
-#from itertools import groupby
-
-#srt_list = sorted(napatie, key=lambda x: x[1]);
-
-#max_list = []
-#upby(srt_list, lambda x: x[1].split('.')[0]):
-	#max_el = max(list(group), key = lambda y: int(y[1].split('.')[1]))
-	#max_list.append(float(max_el[1]))
-				
-#for x in points:
-#	if x == 0 in points:
-#		print('0')
-#	else:
-#		vydelene.append((x+1)/x)
+print('\n---------------------------------\n Medza pevnosti =', round(max(sigma,key=float),5),'MPa \n---------------------------------\n')
+print('\n---------------------------------\n Maximalna deformacia: ', round(max(delta,key=float),5),'\n---------------------------------\n')
 
 
-
-#print("------------------------------------------")
-#for j in vydelene:
-	#print(abs((j)-(j+1))/j)
-	#print(j)
-	#print(j+1)
-
-	
-#for u in vydelene:
-#	if abs((u)-(u+1))/u >= 0.99999:
-#		print("Vaginka")
-#	else:
-#		print("analik")
-
-	
-##########################################
-#def remove_negs(linearita):
-#	for item in linearita:
-#		if item < 0:
-#			linearita.remove(item)
-##########################################
 
 
 #podiel_file=Path(filename).stem + "_linearita.csv"
