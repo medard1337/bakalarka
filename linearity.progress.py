daee0ead0d25d0a35d4a91761a0d483e0f098c3f
medard1337 <miso.florek@gmail.com>
2018-03-05 13:02:34 +0100

    modified:   11375.dat 	modified:   11523.dat 	modified:   12060.dat 	modified:   12060zusl1.dat 	modified:   linearity.progress.py

--- linearity.progress.py
+++ linearity.progress.py
@@ -6,6 +6,8 @@ import csv
 from pathlib import Path
 from math import ceil
 import decimal
+import collections
+import sys, traceback
 
 #otvori subor
 filename=input('Enter filename ')
@@ -16,13 +18,15 @@ vydelene = []
 sigma = []
 delta = []
 
-
 #tato funkcia premiena string na float
 def tofloat(string):
-    if (string):
-        return float(string)
+	if (string):
+		return float(string)
+	return 0.0
 
-    return 0.0
+#invertor osi
+inverter = str(input("Aky format maju vstupne udaje? (A)Napatie/Deformacia alebo (B)Deformacia/Napatie?(A/B)\n"))
+
 
 #tato funkcia prechadza input subor riadok po riadku
 for l in rawfile:
@@ -35,15 +39,30 @@ for l in rawfile:
 		l.rstrip("0.0")
 		#rozdeli hodnoty tam kde su rozdelene tabulatorom
 		split = l.split('\t')
+		if inverter in['A','a']:
+			napatie = tofloat(split[0])
+			deformacia = tofloat(split[1])
+		elif inverter in['B','b']:
+			napatie = tofloat(split[1])
+			deformacia = tofloat(split[0])
+		else:
+			print('Zly vyber kokot')
 		#premeni hodnoty v prvom stlci na float a nazve ich napatie
-		napatie = tofloat(split[0])
+#		napatie = tofloat(split[0])
 		#premeni hodnoty v druhom stlpci na float a nazve ich deformacia
-		deformacia = tofloat(split[1])
+#		deformacia = tofloat(split[1])
 		#pripise hodnoty napatia a deformacie do listu points
 		sigma.append(napatie)
 		delta.append(deformacia)
-		points.append(napatie/deformacia)
+		if deformacia == 0:
+			print('Error: Division by zero\nStlpce vo vstupnom subore su pravdepodobne vymenene.')
+			sys.exit(0)
+		else:
+			points.append(napatie/deformacia)
 
+
+pocitadlo_active = True
+pocitadlo = 0
 #tato funkcia najde youngov modul pruznosti pre kazdy point a porovna ho s nasledujucim 
 for vydelene, i in enumerate(points):
 	if i==0:
@@ -52,24 +71,23 @@ for vydelene, i in enumerate(points):
 		if vydelene>=len(points)-1:
 			print("end")
 		else:
-			if(abs(i-points[vydelene+1]))/i <= 0.03:
+			if(abs(i-points[vydelene+1]))/i <= 0.01:
 				print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+1]))/i,"\t",'\t\tsedi\n')
+				pocitadlo = 0
 			else:
 				print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+1]))/i,'\t','\t\tnesedi\n')
-
-#najde medzu pevnosti a maximalne predlzenie	
-
-print('*Taznost je iba informativna*\n---------------------------------\n Taznost je :',(round(max(delta,key=float),5))*100,'%\n---------------------------------\n')
-
-print('\n---------------------------------\n Medza pevnosti =', round(max(sigma,key=float),5),'MPa \n---------------------------------\n')
-print('\n---------------------------------\n Maximalna deformacia: ', round(max(delta,key=float),5),'\n---------------------------------\n')
-
+				pocitadlo = pocitadlo + 1
+				if pocitadlo == 10 and pocitadlo_active == True:
+					pocitadlo_active = False
+					print(vydelene,"\t",i/100000,"\t\t",(abs(i-points[vydelene+1]))/i,'\t','\t\t*-*-*-*-*-*-*-MEDZA KLZU*-*-*-*-*-*-*-*-*-*\n')
+					medza_klzu = sigma[vydelene]
+					
 
 
+#najde medzu pevnosti a maximalne predlzenie
+print('*Taznost je iba informativna*\n---------------------------------\n Taznost je :',(round(max(delta,key=float),3))*100,'%\n---------------------------------\n')
+print('\n---------------------------------\n Medza pevnosti =', round(max(sigma,key=float),3),'MPa \n---------------------------------\n')
+print('\n---------------------------------\n Maximalna deformacia: ', round(max(delta,key=float),3),'\n---------------------------------\n')
+print('\n---------------------------------\n Medza klzu: ', medza_klzu,'MPa\n---------------------------------\n')
 
-#podiel_file=Path(filename).stem + "_linearita.csv"
-#print(podiel_file)
-#podiel_file.to_csv(podiel_file,index=False)
-#with open(podiel_file, 'w') as output_file:
-#	file_writer = csv.writer(output_file)
-#	file_writer.writerows(points)
+
